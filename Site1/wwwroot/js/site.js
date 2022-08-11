0a3eba6623b039e34115f3a9881f990d613d72de
Regber <regber.888@gmail.com>
2022-08-11 15:15:10 +0300

    Закончено меню в левой части сайта.

--- Site1/wwwroot/js/site.js
+++ Site1/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).on("click", ".left-column>ul>li", function () {
+    let currentElement = $(this);
+    let allElements = $(".left-column>ul>li");
+
+    allElements.each(function () {
+        if ($(this).is(currentElement)) {
+            if (currentElement.hasClass("selected")) {
+                currentElement.removeClass("selected");
+            }
+            else {
+                currentElement.addClass("selected");
+            }
+        }
+        else {
+            $(this).removeClass("selected");
+        }
+    });
+});
+$(document).on("click", ".left-column>ul>li>ul", function (e) {
+    e.stopPropagation()
+});
